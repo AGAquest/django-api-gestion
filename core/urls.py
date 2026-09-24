307ed2dacb74a3f0ff51013716ec63b98b553e8e
Alejandro Aquest <alejandro.aquest@gmail.com>
2026-09-24 20:56:53 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # 1. Importamos la vista de redirección
 from rest_framework.routers import DefaultRouter
 from tickets.views import TicketViewSet
 
@@ -9,4 +10,5 @@ router.register(r'tickets', TicketViewSet, basename='ticket')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', RedirectView.as_view(url='/api/tickets/')),  # 2. Redirigimos la raíz vacía hacia tu API
 ]
