2080d4a7d526d5fdd69b63d7123cc2b8b53b1f49
Alejandro Aquest <alejandro.aquest@gmail.com>
2026-09-24 20:42:19 -0300

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TicketSoporte
-
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TicketSoporte
-        fields = '__all__'
